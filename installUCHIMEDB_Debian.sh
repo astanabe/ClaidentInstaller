5379c90789d5466c3717773fad1dfc754223192b
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2021-02-18 11:34:23 +0900

    Reduce warning messages

--- installUCHIMEDB_Debian.sh
+++ installUCHIMEDB_Debian.sh
@@ -14,7 +14,7 @@ if ! test -e .dairydb; then
 mkdir -p $PREFIX/share/claident/uchimedb 2> /dev/null || sudo mkdir -p $PREFIX/share/claident/uchimedb || exit $?
 wget -nv -c https://github.com/marcomeola/DAIRYdb/raw/master/DAIRYdb_v1.2.4_20200604/DAIRYdb_v1.2.4_20200604_blast/DAIRYdb_v1.2.4_20200604_blast.tgz -O DAIRYdb_v1.2.4_20200604_blast.tgz || exit $?
 tar -xzf DAIRYdb_v1.2.4_20200604_blast.tgz || exit $?
-$PREFIX/share/claident/bin/vsearch --threads 4 --notrunclabels --label_suffix revcomp --fastx_revcomp DAIRYdb_v1.2.4_20200603_blast.fasta --fastaout DAIRYdb_v1.2.4_20200603_blast_rc.fasta || exit $?
+$PREFIX/share/claident/bin/vsearch --notrunclabels --label_suffix revcomp --fastx_revcomp DAIRYdb_v1.2.4_20200603_blast.fasta --fastaout DAIRYdb_v1.2.4_20200603_blast_rc.fasta || exit $?
 cat DAIRYdb_v1.2.4_20200603_blast.fasta DAIRYdb_v1.2.4_20200603_blast_rc.fasta > dairydb1.2.4.fasta || exit $?
 rm -f DAIRYdb_v1.2.4_20200603_blast.fasta DAIRYdb_v1.2.4_20200603_blast_rc.fasta DAIRYdb_v1.2.4_20200603_blast.fasta.n?? DAIRYdb_v1.2.4_20200604_blast.tgz || exit $?
 mv dairydb1.2.4.fasta $PREFIX/share/claident/uchimedb/ 2> /dev/null || sudo mv dairydb1.2.4.fasta $PREFIX/share/claident/uchimedb/ || exit $?
@@ -29,7 +29,7 @@ wget -nv -c https://www.arb-silva.de/fileadmin/silva_databases/release_138_1/Exp
 md5sum -c SILVA_138.1_LSURef_tax_silva.fasta.gz.md5 || exit $?
 rm SILVA_138.1_LSURef_tax_silva.fasta.gz.md5 || exit $?
 gzip -d SILVA_138.1_LSURef_tax_silva.fasta.gz || exit $?
-$PREFIX/share/claident/bin/vsearch --threads 4 --notrunclabels --label_suffix revcomp --fastx_revcomp SILVA_138.1_LSURef_tax_silva.fasta --fastaout SILVA_138.1_LSURef_tax_silva_rc.fasta || exit $?
+$PREFIX/share/claident/bin/vsearch --notrunclabels --label_suffix revcomp --fastx_revcomp SILVA_138.1_LSURef_tax_silva.fasta --fastaout SILVA_138.1_LSURef_tax_silva_rc.fasta || exit $?
 cat SILVA_138.1_LSURef_tax_silva.fasta SILVA_138.1_LSURef_tax_silva_rc.fasta > silva138.1LSUref.fasta || exit $?
 rm -f SILVA_138.1_LSURef_tax_silva.fasta SILVA_138.1_LSURef_tax_silva_rc.fasta || exit $?
 mv silva138.1LSUref.fasta $PREFIX/share/claident/uchimedb/ 2> /dev/null || sudo mv silva138.1LSUref.fasta $PREFIX/share/claident/uchimedb/ || exit $?
@@ -38,7 +38,7 @@ wget -nv -c https://www.arb-silva.de/fileadmin/silva_databases/release_138_1/Exp
 md5sum -c SILVA_138.1_SSURef_tax_silva.fasta.gz.md5 || exit $?
 rm SILVA_138.1_SSURef_tax_silva.fasta.gz.md5 || exit $?
 gzip -d SILVA_138.1_SSURef_tax_silva.fasta.gz || exit $?
-$PREFIX/share/claident/bin/vsearch --threads 4 --notrunclabels --label_suffix revcomp --fastx_revcomp SILVA_138.1_SSURef_tax_silva.fasta --fastaout SILVA_138.1_SSURef_tax_silva_rc.fasta || exit $?
+$PREFIX/share/claident/bin/vsearch --notrunclabels --label_suffix revcomp --fastx_revcomp SILVA_138.1_SSURef_tax_silva.fasta --fastaout SILVA_138.1_SSURef_tax_silva_rc.fasta || exit $?
 cat SILVA_138.1_SSURef_tax_silva.fasta SILVA_138.1_SSURef_tax_silva_rc.fasta > silva138.1SSUref.fasta || exit $?
 rm -f SILVA_138.1_SSURef_tax_silva.fasta SILVA_138.1_SSURef_tax_silva_rc.fasta || exit $?
 mv silva138.1SSUref.fasta $PREFIX/share/claident/uchimedb/ 2> /dev/null || sudo mv silva138.1SSUref.fasta $PREFIX/share/claident/uchimedb/ || exit $?
@@ -55,16 +55,16 @@ perl -i -npe 's/×/XX/g' uchime_reference_dataset_28.06.2017.fasta
 perl -i -npe 's/×/XX/g' untrimmed_sequences/uchime_reference_dataset_untrimmed_28.06.2017.fasta
 perl -i -npe 's/×/XX/g' ITS1_ITS2_datasets/uchime_reference_dataset_ITS1_28.06.2017.fasta
 perl -i -npe 's/×/XX/g' ITS1_ITS2_datasets/uchime_reference_dataset_ITS2_28.06.2017.fasta
-$PREFIX/share/claident/bin/vsearch --threads 4 --notrunclabels --label_suffix revcomp --fastx_revcomp uchime_reference_dataset_28.06.2017.fasta --fastaout uchime_reference_dataset_28.06.2017.rc.fasta || exit $?
+$PREFIX/share/claident/bin/vsearch --notrunclabels --label_suffix revcomp --fastx_revcomp uchime_reference_dataset_28.06.2017.fasta --fastaout uchime_reference_dataset_28.06.2017.rc.fasta || exit $?
 cat uchime_reference_dataset_28.06.2017.fasta uchime_reference_dataset_28.06.2017.rc.fasta > unite20170628.fasta || exit $?
 mv unite20170628.fasta $PREFIX/share/claident/uchimedb/ 2> /dev/null || sudo mv unite20170628.fasta $PREFIX/share/claident/uchimedb/ || exit $?
-$PREFIX/share/claident/bin/vsearch --threads 4 --notrunclabels --label_suffix revcomp --fastx_revcomp untrimmed_sequences/uchime_reference_dataset_untrimmed_28.06.2017.fasta --fastaout untrimmed_sequences/uchime_reference_dataset_untrimmed_28.06.2017.rc.fasta || exit $?
+$PREFIX/share/claident/bin/vsearch --notrunclabels --label_suffix revcomp --fastx_revcomp untrimmed_sequences/uchime_reference_dataset_untrimmed_28.06.2017.fasta --fastaout untrimmed_sequences/uchime_reference_dataset_untrimmed_28.06.2017.rc.fasta || exit $?
 cat untrimmed_sequences/uchime_reference_dataset_untrimmed_28.06.2017.fasta untrimmed_sequences/uchime_reference_dataset_untrimmed_28.06.2017.rc.fasta > unite20170628untrim.fasta || exit $?
 mv unite20170628untrim.fasta $PREFIX/share/claident/uchimedb/ 2> /dev/null || sudo mv unite20170628untrim.fasta $PREFIX/share/claident/uchimedb/ || exit $?
-$PREFIX/share/claident/bin/vsearch --threads 4 --notrunclabels --label_suffix revcomp --fastx_revcomp ITS1_ITS2_datasets/uchime_reference_dataset_ITS1_28.06.2017.fasta --fastaout ITS1_ITS2_datasets/uchime_reference_dataset_ITS1_28.06.2017.rc.fasta || exit $?
+$PREFIX/share/claident/bin/vsearch --notrunclabels --label_suffix revcomp --fastx_revcomp ITS1_ITS2_datasets/uchime_reference_dataset_ITS1_28.06.2017.fasta --fastaout ITS1_ITS2_datasets/uchime_reference_dataset_ITS1_28.06.2017.rc.fasta || exit $?
 cat ITS1_ITS2_datasets/uchime_reference_dataset_ITS1_28.06.2017.fasta ITS1_ITS2_datasets/uchime_reference_dataset_ITS1_28.06.2017.rc.fasta > unite20170628its1.fasta || exit $?
 mv unite20170628its1.fasta $PREFIX/share/claident/uchimedb/ 2> /dev/null || sudo mv unite20170628its1.fasta $PREFIX/share/claident/uchimedb/ || exit $?
-$PREFIX/share/claident/bin/vsearch --threads 4 --notrunclabels --label_suffix revcomp --fastx_revcomp ITS1_ITS2_datasets/uchime_reference_dataset_ITS2_28.06.2017.fasta --fastaout ITS1_ITS2_datasets/uchime_reference_dataset_ITS2_28.06.2017.rc.fasta || exit $?
+$PREFIX/share/claident/bin/vsearch --notrunclabels --label_suffix revcomp --fastx_revcomp ITS1_ITS2_datasets/uchime_reference_dataset_ITS2_28.06.2017.fasta --fastaout ITS1_ITS2_datasets/uchime_reference_dataset_ITS2_28.06.2017.rc.fasta || exit $?
 cat ITS1_ITS2_datasets/uchime_reference_dataset_ITS2_28.06.2017.fasta ITS1_ITS2_datasets/uchime_reference_dataset_ITS2_28.06.2017.rc.fasta > unite20170628its2.fasta || exit $?
 mv unite20170628its2.fasta $PREFIX/share/claident/uchimedb/ 2> /dev/null || sudo mv unite20170628its2.fasta $PREFIX/share/claident/uchimedb/ || exit $?
 cd .. || exit $?
