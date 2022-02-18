1dd4b85e7f238006b07fdf09f5005f5b021f6327
Akifumi S. Tanabe <akifumi.tanabe+github@gmail.com>
2022-02-18 10:33:42 +0900

    Update Claident

--- installClaident_RHEL.sh
+++ installClaident_RHEL.sh
@@ -9,15 +9,15 @@ touch .perlmodules || exit $?
 fi
 # download, and install Claident
 if ! test -e .claident; then
-wget -nv -c https://github.com/astanabe/Claident/archive/v0.9.2022.01.26.tar.gz -O Claident-0.9.2022.01.26.tar.gz || exit $?
-tar -xzf Claident-0.9.2022.01.26.tar.gz || exit $?
-cd Claident-0.9.2022.01.26 || exit $?
+wget -nv -c https://github.com/astanabe/Claident/archive/v0.9.2022.02.18.tar.gz -O Claident-0.9.2022.02.18.tar.gz || exit $?
+tar -xzf Claident-0.9.2022.02.18.tar.gz || exit $?
+cd Claident-0.9.2022.02.18 || exit $?
 make PREFIX=$PREFIX -j8 || exit $?
 make PREFIX=$PREFIX install 2> /dev/null || sudo make PREFIX=$PREFIX install || exit $?
 cp $PREFIX/share/claident/.claident ~/.claident || exit $?
 cd .. || exit $?
-rm -rf Claident-0.9.2022.01.26 || exit $?
-rm -f Claident-0.9.2022.01.26.tar.gz || exit $?
+rm -rf Claident-0.9.2022.02.18 || exit $?
+rm -f Claident-0.9.2022.02.18.tar.gz || exit $?
 touch .claident || exit $?
 fi
 # download , compile, and install Swarm
