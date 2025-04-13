596491d689e871e67c6fc99f1b8fce6f75dc5d35
astanabe <akifumi.tanabe@gmail.com>
2025-04-13 20:21:42 +0900

    The installer scripts have been updated.

--- installClaident_Debian.sh
+++ installClaident_Debian.sh
@@ -5,14 +5,14 @@ fi
 mkdir -p $PREFIX || sudo mkdir -p $PREFIX || exit $?
 # install Perl modules
 if ! test -e .perlmodules; then
-wget http://xrl.us/cpanm || exit $?
+wget -c http://xrl.us/cpanm || exit $?
 chmod 755 cpanm || exit $?
 if test -w $PREFIX; then
 mkdir -p $PREFIX/share/claident || exit $?
-./cpanm -L $PREFIX/share/claident Math::CDF || exit $?
+./cpanm -nfL $PREFIX/share/claident Math::CDF || exit $?
 else
 sudo -E mkdir -p $PREFIX/share/claident || exit $?
-sudo -E ./cpanm -L $PREFIX/share/claident Math::CDF || exit $?
+sudo -E ./cpanm -nfL $PREFIX/share/claident Math::CDF || exit $?
 fi
 perl -I$PREFIX/share/claident/lib/perl5 -e 'use File::Copy::Recursive;use DBD::SQLite;use Math::BaseCnv;use Math::CDF' || exit $?
 rm -f cpanm || exit $?
